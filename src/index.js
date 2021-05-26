ea21ed800c0724136eb2817572c6a3135c0902ba
Code sync <code-sync@criodo.com>
2021-05-26 20:26:16 +0530

    code sync commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <App/>,document.getElementById
     </BrowserRouter>
   </React.StrictMode>,
 );
